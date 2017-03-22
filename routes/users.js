a8c5c0c7325771905411b39da59f13e290aa4630
Tommy2017 <sacredzeus@outlook.com>
2017-03-22 15:50:51 +0800

    Access limitation, Finished

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var User  = require("../models/User");
 var util  = require("../util");
 
 // Index
-router.route("/").get(function(req, res){
+router.get("/", util.isLoggedin, function(req, res){
     User.find({})
         .sort({username:1})
         .exec(function(err, users){
@@ -35,7 +35,7 @@ router.post("/", function(req, res){
 });
 
 // show
-router.get("/:username", function(req, res){
+router.get("/:username", util.isLoggedin, function(req, res){
     User.findOne({username:req.params.username}, function(err, user){
         if(err) return res.json(err);
         res.render("users/show", {user:user});
@@ -43,7 +43,7 @@ router.get("/:username", function(req, res){
 });
 
 // edit
-router.get("/:username/edit", function(req, res){
+router.get("/:username/edit", util.isLoggedin, checkPermission, function(req, res){
     var user = req.flash("user")[0];
     var errors = req.flash("errors")[0] || {};
     if(!user){
@@ -57,7 +57,7 @@ router.get("/:username/edit", function(req, res){
 });
 
 // update
-router.put("/:username",function(req, res, next){
+router.put("/:username", util.isLoggedin, checkPermission, function(req, res, next){
     User.findOne({username:req.params.username})
         .select({password:1})
         .exec(function(err, user){
@@ -93,4 +93,14 @@ function parseError(errors){
         parsed.unhandled = JSON.stringify(errors);
     }
     return parsed;
+}
+
+// private functions
+function checkPermission(req, res, next){
+    User.findOne({username:req.params.username}, function(err, user){
+        if(err) return res.json(err);
+        if(user.id != req.user.id) return util.noPermission(req, res);
+
+        next();
+    });
 }
